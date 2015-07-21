e2ba2b884c61789cfa65ac16978f098429a4d8ce
Dan Chao <dan@play-i.com>
2015-07-21 14:33:05 -0700

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# Numer
+
+Number to datetime helpers. This adds a series of methods onto numbers to turn datetime intervals into millisecond values.
+
+NOTE: This adds methods onto `Number#prototype`.
+
+### Example
+
+```js
+(3).minutes();
+// => 180000
+(3).days();
+// => 259200000
+```
+
+### Installation
+
+Install this package through npm.
+
+```bash
+npm install numer
+```
+
+Then, either include the source for this project in a script tag, or you may also use this as a CommonJS module and `require` it into your project. It only needs to be required once, and the module does not export anything.
---
+++ index.js
@@ -0,0 +1,37 @@
+(function(){
+  var ONE_SECOND = 1000;
+  var ONE_MINUTE = ONE_SECOND * 60;
+  var ONE_HOUR = ONE_MINUTE * 60;
+  var ONE_DAY = ONE_HOUR * 24;
+  var ONE_WEEK = ONE_DAY * 7;
+  var ONE_MONTH = ONE_WEEK * 4;
+  var ONE_YEAR = ONE_DAY * 365;
+
+  Number.prototype.seconds = function(){
+    return this * ONE_SECOND;
+  };
+
+  Number.prototype.minutes = function(){
+    return this * ONE_MINUTE;
+  };
+
+  Number.prototype.hours = function(){
+    return this * ONE_HOUR;
+  };
+
+  Number.prototype.days = function(){
+    return this * ONE_DAY;
+  };
+
+  Number.prototype.weeks = function(){
+    return this * ONE_WEEK;
+  };
+
+  Number.prototype.months = function(){
+    return this * ONE_MONTH;
+  };
+
+  Number.prototype.years = function(){
+    return this * ONE_YEAR;
+  };
+})();
